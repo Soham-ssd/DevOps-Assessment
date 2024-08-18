7d044747b32bff2194fb103e3464bd1f45b4a535
Soham Dabadgaonkar <130518761+Soham-ssd@users.noreply.github.com>
2024-08-19 05:26:38 +0530

    Delete terraform/cicd.sh

--- terraform/cicd.sh
+++
@@ -1,4 +0,0 @@
-docker build -t notificationapp .
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REPO_URL
-docker tag notificationapp $ECR_REPO_URL:latest
-docker push $ECR_REPO_URL:latest
